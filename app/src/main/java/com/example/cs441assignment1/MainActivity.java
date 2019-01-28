653a7ece8ae879e93010cf84e03d5a4d64fee6ff
vladimirmalcevic@ZANZIBAR <vladimirmalcevic@gmail.com>
2019-01-27 22:49:46 -0500

    Added button for Hello World
    Added a button as well as button event listners. Unable to change the text back on button lift.

--- app/src/main/java/com/example/cs441assignment1/MainActivity.java
+++ app/src/main/java/com/example/cs441assignment1/MainActivity.java
@@ -5,11 +5,15 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +28,30 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
+            }
+        });
+
+        changeText();
+    }
+
+    private void changeText(){
+        final TextView changetext = (TextView) findViewById(R.id.text_to_change);
+
+        final Button changeTextButton = (Button) findViewById(R.id.change_text_button);
+
+        changeTextButton.setOnTouchListener(new View.OnTouchListener() {
+
+            @Override
+            public boolean onTouch(View view, MotionEvent event) {
+
+                if(event.getButtonState() == MotionEvent.ACTION_DOWN){  //need to find a way to change the text back so that the text only changes for when the button is pressed down
+                    changetext.setText("Hello world and good Morning!");
+                    return true;
+                } else if(event.getButtonState() == MotionEvent.ACTION_UP){
+                    changetext.setText("Press the button again.");
+                    return true;
+                }
+                return false;
             }
         });
     }
